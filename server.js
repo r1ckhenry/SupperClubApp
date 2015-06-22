3475f5c517158a1b9196d8a7b0ff3fbbc40f43b4
Anna Gaughan <agaughan@gmail.com>
2015-06-22 14:38:54 +0100

    index page working with node

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
-
+var server = require('http').createServer(app);
+// var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 
+app.set('views', './views');
 app.set('view engine', 'ejs');
-app.set('morgan');
+
+app.use(morgan('dev'));
+// app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
+  // res.send('pants')
   res.render('index')
 });
 
+
+
 app.listen(port, function(req,res){
-  console.log('Now running on port ' + port)
+  console.log('arse ' + port)
 });
 
 
