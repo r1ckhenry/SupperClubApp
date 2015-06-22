a9a125fcda3b7ced4e6f8c1e0d132a4c03f1a250
Anna Gaughan <agaughan@gmail.com>
2015-06-22 16:31:34 +0100

    input being recorded in db

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,17 +1,31 @@
-var Supper = Supper|| {}
+// console.log('apple')
+// var Supper = Supper|| {}
 
-$(document).ready(function(){
-  Supper.all
-  
-});
 
-supper = {
-  all: function(){
-    $('#form-submit').on('submit', function(e){
-      e.preventDefault();
-      console.log('working')
-      Supper.create($(this).serialize())
-      // console.log('working')
-    });
-    }
-  }
+// $(document).ready(function(){
+//   // Supper.all
+//   // console.log('banana')
+// // });
+
+// // Supper = {
+// //   all: function(){
+// //     console.log('popchips');
+//     $('#search-form').on("submit", function(e){
+//       e.preventDefault();
+//       console.log("pen")
+//       var $form = $(this)
+//       // location = $form.find(location).val(),
+
+//       // var posting = $.post(url, {s: term})
+
+//       // posting.done(function(data){
+//       //   var content = $(data).find("#content");
+//       //   $("#result").empty().append(content);
+//       // })
+//       // Supper.create($(this).serialize())
+//       // debugger
+//       // console.log('working')
+//     });
+//     // }
+//   // }
+// });
--- server.js
+++ server.js
@@ -2,27 +2,27 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var server = require('http').createServer(app);
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
-// app.use(bodyParser.json());
+app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  // res.send('pants')
   res.render('index')
 });
 
-app.get('/search', function(req, res){
-  res.render('search')
-})
-
 app.get('/searchresults', function(req, res){
   res.render('searchresults')
+}); 
+
+app.post('/', function(req,res){
+  var newSupper = req.body;
+  console.log(newSupper);
 })
 
 
