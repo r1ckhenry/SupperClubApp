e261bab427f20e422d56a2cec95a65e8352e0949
Anna Gaughan <agaughan@gmail.com>
2015-06-23 17:04:13 +0100

    schema uptodate

--- app/models/seeds.js
+++ app/models/seeds.js
@@ -20,8 +20,15 @@ db.Supper.create({
   menu: {
     veggie: true,
     vegan: false,
-    dishes: ["bangers and mash", "chocolate cake"]
-  }
+    dishes: ["bangers and mash", "chocolate cake"],
+    cuisine: ["Italien"],
+    drinks: ["wine"]
+  },
+  guest: 12,
+  description: "Donut marzipan donut apple pie. Jelly-o topping sesame snaps oat cake cake caramels. Jelly-o marzipan jujubes bear claw jujubes fruitcake cupcake. Icing lemon drops macaroon bonbon tootsie roll. Marshmallow sesame snaps cupcake gummies cookie caramels sugar plum. Cookie cake chupa chups sugar plum cotton candy jelly-o lemon drops sesame snaps candy canes. Cotton candy tiramisu halvah. Croissant donut marzipan dragée powder. Apple pie toffee cheesecake tart. Jelly-o cotton candy chocolate candy bonbon marzipan chocolate cake. Bear claw marzipan gummi bears gingerbread lemon drops tart pie. Cake chocolate bar cake jelly-o. Sweet roll gingerbread sugar plum. Oat cake carrot cake jujubes caramels chocolate bar.",
+  dressCode: "casual",
+  reviews: [8,9]
+
 }, function(err, supper){
   console.log("********");
   console.log(supper);
@@ -30,22 +37,23 @@ db.Supper.create({
 db.User.create({
   name: 'Anna Gaughan',
   password: 'anna',
-  email: 'anna@anna.com'
+  email: 'anna@anna.com',
+  address: {
+    firstLine: '34',
+    secondLine: 'Sloe Street',
+    city: 'London',
+    postCode: 'EC6 4BQ'
+  }
 }, function(err, user){
   console.log("inside user create");
   console.log(user);
 });
 
-db.Supper.create({
-  description: "Yummy"
-}, function(err, supper){
-  console.log("inside supper create");
-  console.log(supper);
-});
 
 
 
-// console.log('Seeded')
+
+console.log('Seeded')
 
 
 
