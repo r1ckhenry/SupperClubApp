af290c2e1089909bfde311adb711781f0ac7f257
Tobia Cassandro <tobiacassandro@gmail.com>
2015-06-23 14:22:26 +0100

    added form for adding suppers and RESTful route for suppers

--- app/suppers-server.js
+++ app/suppers-server.js
@@ -2,10 +2,30 @@ module.exports = function(app, db) {
 
   app.get('/suppers', function(req, res) {
     db.Supper.find({}, function (err, suppers){
-      var hello = suppers;
-      res.send(suppers);
-      console.log(suppers);
-     })
+      res.render('suppers.ejs', {
+        supper: suppers
+      });
+    })
+  })
+
+  app.post('/suppers', function(req, res) {
+    console.log(req.body);
+    db.Supper.create(req.body, function(err, supper){
+      res.send(201, supper);
+    })
+  })
+
+  app.get('/suppers/new', function(req, res){
+   res.render('suppers/new.ejs');
   });
+
+  app.delete("/suppers/:id", function(req, res){
+    var supperId = req.params.id;
+    db.Supper.findByIdAndRemove({
+      _id: supperId
+    }, function(err, supper){
+      res.send(204);
+    })
+  })
 
 }
--- public/javascript/suppers.js
+++ public/javascript/suppers.js
@@ -1,4 +1,30 @@
-$(document).ready(function(){
-  
+// var Supper = Supper|| {};
+// var View = View || {};
 
-});
+// $(document).ready(function(){
+
+//   Supper.all();
+
+// });
+
+// Supper = {
+//   all: function(){
+//     $.get('/suppers', function(response) {
+//       console.log(response);
+//       $.each(response, function(index, blog) {
+//         console.log(supper);
+//       })
+//     });
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+// });
