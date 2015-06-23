2977902e8a2ef5831fed590c7ef8c234cdb946e9
Richard Henry <r.henry1989@gmail.com>
2015-06-23 15:55:07 +0100

    c r and d routes created

--- app/suppers-server.js
+++ app/suppers-server.js
@@ -8,18 +8,41 @@ module.exports = function(app, db) {
     })
   })
 
-  app.post('/suppers', function(req, res) {
-    console.log(req.body);
-    db.Supper.create(req.body, function(err, supper){
-      res.send(201, supper);
+  app.get('/suppers/:id', function(req, res) {
+    db.Supper.findById(req.params.id, function(err, supper){
+      res.render('suppers/show.ejs', {
+        supper: supper
+      });
     })
+  });
+
+  app.post('/suppers', function(req, res) {
+    var sup = req.body;
+    db.Supper.create({
+      description : sup.description,
+      dressCode: sup.dressCode,
+      address: {
+        firstLine: sup.firstLine,
+        secondLine: sup.secondLine,
+        city: sup.city,
+        postCode: sup.postCode
+      },
+      menu: {
+        dishes: sup.dishes,
+        cuisine: sup.cuisine,
+        drinks: sup.drinks
+      }
+    }, function(err, supper){
+      res.redirect('/suppers');
+    });
   })
 
   app.get('/suppers/new', function(req, res){
    res.render('suppers/new.ejs');
   });
 
-  app.delete("/suppers/:id", function(req, res){
+  //DELETE METHOD
+  app.post("/suppers/:id", function(req, res){
     var supperId = req.params.id;
     db.Supper.findByIdAndRemove({
       _id: supperId
