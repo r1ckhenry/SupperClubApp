a7c1dd74b204988c7a78b5dfb9b3ce87bbe7f955
Anna Gaughan <agaughan@gmail.com>
2015-06-25 15:02:30 +0100

    clearing booking form when dinner booked

--- public/javascript/attending.js
+++ public/javascript/attending.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
   var pathname = window.location.pathname;
   $('.attending').on('click', function(e){
     e.preventDefault();
+    $('.booking-content').empty()
     $('#notify').append("You're going to this supper!");
     
     $.get(pathname, function(response){
