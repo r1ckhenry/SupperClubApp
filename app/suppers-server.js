870df379dd752e7d0977ea0fe3ad2d4f30a507c3
Richard Henry <r.henry1989@gmail.com>
2015-06-25 21:06:33 +0100

    final fix commit

--- app/suppers-server.js
+++ app/suppers-server.js
@@ -8,14 +8,4 @@ module.exports = function(app, db) {
     })
   })
 
-  //DELETE METHOD
-  app.post("/suppers/:id", function(req, res){
-    var supperId = req.params.id;
-    db.Supper.findByIdAndRemove({
-      _id: supperId
-    }, function(err, supper){
-      res.send(204);
-    })
-  })
-
 }
